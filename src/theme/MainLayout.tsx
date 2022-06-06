a835917a1b989a02a19d23ad38b088ca58bc5d6e
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-06 19:16:09 -0300

    Create Main Layout Component

--- src/theme/MainLayout.tsx
+++ src/theme/MainLayout.tsx
@@ -16,29 +16,21 @@ const MainLayout = () => {
   return (
     <IonTabs>
       <IonRouterOutlet>
-        <Route exact path="/tab1">
-          <Tab1 />
-        </Route>
-        <Route exact path="/tab2">
-          <Tab2 />
-        </Route>
-        <Route path="/tab3">
-          <Tab3 />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/tab1" />
-        </Route>
+        <Route path="/dashboard/tab1" component={Tab1} />
+        <Route path="/dashboard/tab2" component={Tab2} />
+        <Route path="/dashboard/tab3" component={Tab3} />
+        <Redirect path="/dashboard" to="/dashboard/tab1" exact />
       </IonRouterOutlet>
       <IonTabBar slot="bottom">
-        <IonTabButton tab="tab1" href="/tab1">
+        <IonTabButton tab="tab1" href="/dashboard/tab1">
           <IonIcon icon={triangle} />
           <IonLabel>Tab 1</IonLabel>
         </IonTabButton>
-        <IonTabButton tab="tab2" href="/tab2">
+        <IonTabButton tab="tab2" href="/dashboard/tab2">
           <IonIcon icon={ellipse} />
           <IonLabel>Tab 2</IonLabel>
         </IonTabButton>
-        <IonTabButton tab="tab3" href="/tab3">
+        <IonTabButton tab="tab3" href="/dashboard/tab3">
           <IonIcon icon={square} />
           <IonLabel>Tab 3</IonLabel>
         </IonTabButton>
