8c559b68f09f32c8a59a370d7b0b123e7bea92ba
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-09 22:26:56 -0300

    Create Login page - Create Button component. - Create LabeledTextField component. - Create H1 and Small components.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,73 @@
 import { IonContent, IonPage } from "@ionic/react";
-import Header from "../components/Header";
+import { ValidationErrors } from "final-form";
+import { Form } from "react-final-form";
+import { useHistory } from "react-router";
+import Button from "../components/Button";
+import { H1, Small } from "../components/fonts";
+import LabeledTextField from "../components/LabeledTextField";
+import "../styles/pages/Login.css";
 
 const Login: React.FC = () => {
+  const router = useHistory();
+
+  const onSubmit = (values: any) => {
+    // check if there is any validation error
+    if (Object.keys(validation(values)).length === 0) {
+      login(values, router);
+    }
+  };
+
   return (
     <IonPage>
-      <Header title="Iniciar Sesión" />
       <IonContent fullscreen>
-        <div className=""></div>
+        <div className="page">
+          <div className="header">
+            <H1>Inicia Sesión</H1>
+            <Small>Ingresa tus credenciales para acceder a tu cuenta</Small>
+          </div>
+          <Form
+            onSubmit={() => {}}
+            validate={validation}
+            render={({ handleSubmit, submitting, pristine, values }) => (
+              <form onSubmit={handleSubmit} className="form">
+                <div className="formBody">
+                  <LabeledTextField name="email" label="Email" />
+                  <LabeledTextField
+                    name="password"
+                    label="Contraseña"
+                    type="password"
+                  />
+                </div>
+                <Button
+                  text="Entrar"
+                  type="submit"
+                  onClick={() => onSubmit(values)}
+                  disabled={submitting || pristine}
+                />
+              </form>
+            )}
+          />
+        </div>
       </IonContent>
     </IonPage>
   );
+};
+
+const validation = (values: any) => {
+  const errors: ValidationErrors = {};
+  if (!values.email) {
+    errors.email = "Required";
+  }
+  if (!values.password) {
+    errors.password = "Required";
+  }
+  return errors;
+};
+
+const login = (values: any, router: any) => {
+  if (values.email === "mail@mail.com" && values.password === "hola123") {
+    router.push("/dashboard");
+  }
 };
 
 export default Login;
